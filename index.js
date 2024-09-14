b32a0d572a855c221e657bf93b2a8d8aaf64de61
Kamal-lochanDash <kamallochandash04@gmail.com>
2024-09-14 19:15:12 +0530

    Minor changes

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.use((req,res,next)=>{
 
 //# routs
 app.get("/api/users",(req,res)=>{
+    res.setHeader('x-myName','kamal lochan dash')
+    console.log(req.headers);
    return res.json(users);
 })
 
@@ -48,11 +50,16 @@ app.get("/users",(req,res)=>{
 app.post("/api/users",(req,res)=>{
     //create a new user
    // const body=req.query;
+
    const body=req.body;
+
+   if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+    return res.status(400).json({msg:"all filelds are required"});
+   }
     users.push({id: users.length+1,...body,})
     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(error,data)=>{
         console.log("Body",body)
-        return res.json({status:"Success",id:users.length})
+        return res.status(201).json({status:"Success",id:users.length})
     })
 
    
@@ -64,8 +71,11 @@ app.route("/api/users/:id")
 .get((req,res)=>{
     console.log("i am in the get route and my name is "+req.myUsername)
     const id=Number(req.params.id)
-   
-    const user=users.find((user)=>user.id===id);
+   const user=users.find((user)=>user.id===id);
+
+   if(!user){
+    return res.status(404).json({msg: "User not found"})
+   }
     res.json(user)
    
    
